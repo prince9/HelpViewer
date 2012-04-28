35ae6c3ef804016efd7f64778b05eddceb2e4acc
MAYU <mtsuzaka@rb3.so-net.ne.jp>
2012-04-29 08:23:26 +0900

    Initial Commit

---
+++ helpViewer/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  helpViewer
+//
+//  Created by 真有 津坂 on 12/04/29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ helpViewer/SecondViewController.m
@@ -0,0 +1,34 @@
+//
+//  SecondViewController.m
+//  helpViewer
+//
+//  Created by 真有 津坂 on 12/04/29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
